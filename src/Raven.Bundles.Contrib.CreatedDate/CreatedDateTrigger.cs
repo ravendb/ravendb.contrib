7034d7366cd75bcc075757a6d7bb67265c9c6bd6
Matt Johnson <mj1856@hotmail.com>
2012-12-20 17:02:02 -0700

    Adding CreatedDate trigger and listener

---
+++ src/Raven.Bundles.Contrib.CreatedDate/CreatedDateTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Raven.Abstractions;
+using Raven.Abstractions.Data;
+using Raven.Database.Plugins;
+using Raven.Json.Linq;
+
+namespace Raven.Bundles.CreatedDate
+{
+	/// <summary>
+	/// A server-side trigger that adds a Created metadata value to every document being stored.
+	/// Does not need to be "activated" like other bundles.  Just drop it into the Plugins directory and it will be active on all new documents.
+	/// </summary>
+	public class CreatedDateTrigger : AbstractPutTrigger
+	{
+		public override void OnPut(string key, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
+		{
+			RavenJToken lastModified;
+			if (!metadata.TryGetValue("Last-Modified", out lastModified))
+				metadata.Add("Created", SystemTime.UtcNow);
+		}
+	}
+}
---
+++ src/Raven.Client.Contrib/Listeners/CreatedDateListener.cs
@@ -0,0 +1,24 @@
+﻿using Raven.Abstractions;
+using Raven.Abstractions.Data;
+using Raven.Json.Linq;
+
+namespace Raven.Client.Listeners
+{
+	/// <summary>
+	/// A client-side listener that adds a Created metadata value to every document being stored.
+	/// Uses the clock on the client.  For better precision, use a server-side trigger instead.
+	/// </summary>
+	public class CreatedDateListener : IDocumentStoreListener
+	{
+		public bool BeforeStore(string key, object entityInstance, RavenJObject metadata, RavenJObject original)
+		{
+			RavenJToken lastModified;
+			if (!metadata.TryGetValue(Constants.LastModified, out lastModified))
+				metadata.Add("Created", SystemTime.UtcNow);
+
+			return true;
+		}
+
+		public void AfterStore(string key, object entityInstance, RavenJObject metadata) { }
+	}
+}
