fb3d70bc92b6a5bd2219ad28a4bfe83255dda0e7
Matt Johnson <mj1856@hotmail.com>
2012-12-21 11:22:45 -0700

    Tests for Created Date trigger

---
+++ src/Raven.Bundles.Contrib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Raven.Bundles.Contrib.Tests")]
+[assembly: AssemblyDescription("Tests for Contrib Bundles.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Raven.Bundles.Contrib.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2012 - RavenDB Community Members")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
