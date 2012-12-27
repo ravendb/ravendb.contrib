a37d0c8299cea05da727dc37d0eedf0093ec33ab
Matt Johnson <mj1856@hotmail.com>
2012-12-27 12:31:48 -0700

    Updating assembly info & sharing much of it.
    Version numbers would normally be shared, but since we have several
    separate projects that may be versioned separately, I left the version
    numbers in each project's own assembly info.

--- src/Raven.Client.Contrib/Properties/AssemblyInfo.cs
+++ src/Raven.Client.Contrib/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Raven.Client.Contrib")]
 [assembly: AssemblyDescription("Contributed addons for the RavenDB Client Libraray")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Raven.Client.Contrib")]
-[assembly: AssemblyCopyright("Copyright © 2012 - RavenDB Community Members")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
