e453d2026e6651bf69710249e023f1d5db1f3d13
Matt Johnson <mj1856@hotmail.com>
2012-12-28 10:34:18 -0700

    minor update to configuration

--- src/Raven.Bundles.Contrib.ClockDocs/ClockDocsHelper.cs
+++ src/Raven.Bundles.Contrib.ClockDocs/ClockDocsHelper.cs
@@ -6,10 +6,10 @@ namespace Raven.Bundles.ClockDocs
     public static class ClockDocsHelper
     {
         /// <summary>
-        /// Registers the ClockDocsBundle in the configuration of an embedded database.
+        /// Registers the ClockDocsBundle in the configuration of a database instance.
         /// </summary>
         /// <param name="configuration"></param>
-        public static void RegisterBundle(RavenConfiguration configuration)
+        public static void RegisterBundle(InMemoryRavenConfiguration configuration)
         {
             configuration.Catalog.Catalogs.Add(new AssemblyCatalog(typeof(ClockDocsHelper).Assembly));
         }
