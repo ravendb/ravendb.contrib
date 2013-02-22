e015447ad862764088ed6a2dba81542ad58daf37
Troy Zarger <troy.zarger@webtechedgeinc.com>
2013-02-22 11:43:26 -0500

    StandardNoStops Analyzer
    I added the Lucene.Net Package, however this may need to be updated to
    keep in sync with RavenDB's version of Lucene.

---
+++ src/Raven.Contrib.Analyzers.StandardNoStops/StandardNoStopsAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Util;
+
+namespace Raven.Contrib.Analyzers.StandardNoStops
+{
+	public class StandardNoStopsAnalyzer : StandardAnalyzer
+	{
+		public static readonly ISet<string> StopWordsSet;
+
+		public StandardNoStopsAnalyzer( Version matchVersion ) : base( matchVersion, StopWordsSet ) { }
+		public StandardNoStopsAnalyzer( Version matchVersion, ISet<string> stopWords ) : base( matchVersion, stopWords ) { }
+		public StandardNoStopsAnalyzer( Version matchVersion, FileInfo stopwords ) : base( matchVersion, stopwords ) { }
+		public StandardNoStopsAnalyzer( Version matchVersion, TextReader stopwords ) : base( matchVersion, stopwords ) { }
+
+		static StandardNoStopsAnalyzer() { StopWordsSet = new HashSet<string>(); }
+	}
+}
