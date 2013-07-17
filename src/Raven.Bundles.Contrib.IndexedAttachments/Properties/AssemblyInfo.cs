ba2c538ec5920781c833cc290e2d77df3df0e2ae
Matt Johnson <mj1856@hotmail.com>
2013-07-16 17:39:17 -0700

    Remove Indexed Attachments Bundle
    Moved to https://github.com/mj1856/RavenDB-IndexedAttachments

--- src/Raven.Bundles.Contrib.IndexedAttachments/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Raven.Bundles.Contrib.IndexedAttachments")]
-[assembly: AssemblyDescription("Plugin for RavenDB that extracts text from attachments so they can be indexed.")]
-[assembly: AssemblyVersion("2.0.4.1")]
-[assembly: AssemblyFileVersion("2.0.4.1")]
