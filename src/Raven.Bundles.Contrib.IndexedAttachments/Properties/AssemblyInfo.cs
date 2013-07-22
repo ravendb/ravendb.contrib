447866f769af2d84cb18e7f31e79417d4236a738
Matt Johnson <mj1856@hotmail.com>
2013-07-22 11:07:21 -0700

    Remove IndexedAttachments and ClockDocs bundles
    Moved to https://github.com/mj1856

--- src/Raven.Bundles.Contrib.IndexedAttachments/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Raven.Bundles.Contrib.IndexedAttachments")]
-[assembly: AssemblyDescription("Plugin for RavenDB that extracts text from attachments so they can be indexed.")]
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
-[assembly: AssemblyInformationalVersion("2.5.0-Unstable")]
-
