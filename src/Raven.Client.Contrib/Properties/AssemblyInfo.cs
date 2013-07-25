20b2cc7e8235541928714fade925b7ba06e35fcc
Matt Johnson <mj1856@hotmail.com>
2013-07-25 13:20:18 -0700

    Fix version numbers
    oops - skipped a version!

--- src/Raven.Client.Contrib/Properties/AssemblyInfo.cs
+++ src/Raven.Client.Contrib/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("Raven.Client.Contrib")]
 [assembly: AssemblyDescription("Contributed addons for the RavenDB Client Library")]
-[assembly: AssemblyVersion("2.5.1")]
-[assembly: AssemblyFileVersion("2.5.1.2")]
-[assembly: AssemblyInformationalVersion("2.5.1")]
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.0.1")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
