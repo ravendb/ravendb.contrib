eb53f262345acf2affee5e779e05b15957ee4df4
Matt Johnson <mj1856@hotmail.com>
2013-07-25 13:04:23 -0700

    fix typos

--- src/Raven.Client.Contrib.Tests/Properties/AssemblyInfo.cs
+++ src/Raven.Client.Contrib.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Raven.Client.Contrib.Tests")]
-[assembly: AssemblyDescription("Contributed addons for the RavenDB Client Libraray - Unit Tests")]
+[assembly: AssemblyDescription("Contributed addons for the RavenDB Client Library - Unit Tests")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
--- src/Raven.Client.Contrib/Properties/AssemblyInfo.cs
+++ src/Raven.Client.Contrib/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Raven.Client.Contrib")]
-[assembly: AssemblyDescription("Contributed addons for the RavenDB Client Libraray")]
+[assembly: AssemblyDescription("Contributed addons for the RavenDB Client Library")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
