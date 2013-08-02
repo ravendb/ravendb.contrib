26ca0006974e7ea7e50160f73519b0d497cbec32
Matt Johnson <mj1856@hotmail.com>
2013-08-02 11:44:16 -0700

    Adding unit tests for server status

---
+++ src/Raven.Client.Contrib.Tests/ServerStatusExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Raven.Abstractions.Data;
+using Raven.Tests.Helpers;
+using Xunit;
+
+namespace Raven.Client.Contrib.Tests
+{
+    public class ServerStatusExtensionsTests : RavenTestBase
+    {
+        [Fact]
+        public void Can_Get_Server_Build_Number()
+        {
+            using (var store = NewDocumentStore())
+            {
+                BuildNumber buildNumber;
+                var ok = store.TryGetServerVersion(out buildNumber);
+                Assert.True(ok);
+                Debug.WriteLine(buildNumber.ProductVersion);
+                Debug.WriteLine(buildNumber.BuildVersion);
+            }
+        }
+
+        [Fact]
+        public void Can_Get_Server_Online_Status()
+        {
+            using (var store = NewDocumentStore())
+            {
+                Assert.True(store.IsServerOnline());
+            }
+        }
+    }
+}
